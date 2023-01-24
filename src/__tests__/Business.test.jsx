085cd96449dd73144d84c599096f82b18d894cfc
khaya05 <khayamnyandu5@gmail.com>
2023-01-24 14:52:23 +0200

    added tests

--- src/__tests__/Business.test.jsx
+++ src/__tests__/Business.test.jsx
@@ -1,7 +1,19 @@
-import { Business } from "../components"
+import { render, screen } from '@testing-library/react';
+import { Business } from '../components';
 
 describe('Business', () => {
+  const text =
+    'At the core of our platform is the technical infrastructure APIs that connect consumers. Our innovative product provides key insights for businesses and individuals, as well as robust reporting for traditional financial institutions and developers.';
+  
   it('renders heading', () => {
-    // render
-  })
-})
+    render(<Business />);
+    const heading = screen.getByRole('heading');
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('renders paragraph', () => {
+    render(<Business />);
+    const paragraph = screen.getByText(text);
+    expect(paragraph).toBeInTheDocument();
+  });
+});
